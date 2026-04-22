26cd811cc054c9f8989644d6d89141861658be6f
MrPicometre <samrud04@gmail.com>
2026-04-22 21:36:38 +0530

    login page
    color, frames, logo, title

--- frontend/main.py
+++ frontend/main.py
@@ -1,9 +1,23 @@
 import tkinter as tk
-import mysql.connector as psq
-
 
 def main():
-    pass
+    root = tk.Tk()
+    root.title("Cat-Nip")
+    root.geometry("800x600")
+    root.iconphoto(False, tk.PhotoImage(file="C:/Users/Dr.Satyan/Documents/Samrud/Sam Codes/cat-nip/frontend/assets/catnipico.png"))
+    
+    main_frame = tk.Frame(root, bg="white")
+    main_frame.pack(fill=tk.BOTH, expand=True)
+    
+    left_frame = tk.Frame(main_frame, bg="#804000", width=450)
+    left_frame.pack(side=tk.LEFT, fill=tk.BOTH)
+    left_frame.pack_propagate(False)  
+
+    right_frame = tk.Frame(main_frame, bg="#e6ccb3", width=450)
+    right_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+    right_frame.pack_propagate(False)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
