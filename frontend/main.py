64118b94bfd03d0f2a3c82c2addc5464612cb12c
aadhithiyago-cpu <aadhithiyago@gmail.com>
2026-04-23 14:51:40 +0530

    Update main.py
    button added hehe

--- frontend/main.py
+++ frontend/main.py
@@ -18,7 +18,10 @@ def main():
     right_frame.pack_propagate(False)
 
     tk.Label(left_frame, text="Cat-Nip", fg="white", bg="#804000", font=("Arial", 24)).pack(pady=20)
-
+    button_frame= tk.Frame(left_frame, bg="#804000")
+    button_frame.pack(pady=20)
+    tk.Button(button_frame, text="Start", width=15, height=2).pack(pady=10 )
+    
     root.mainloop()
 
 if __name__ == "__main__":
